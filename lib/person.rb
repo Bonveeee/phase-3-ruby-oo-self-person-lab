eb51c32f6784e7fe6119bf77b7b136d638fe3c2d
bonveeee <bonochieng@gmail.com>
2022-08-20 22:48:50 +0300

    commit tree 4c885902bc67fddc7d25928a8b3194865f39fdeb

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,22 @@
 # your code goes here
+class Person
+attr_reader :name
+
+def initialize(name)
+    @name = name
+end
+
+
+def bank_account
+    @bank_account = bank_account
+end
+
+def happiness
+    @happiness = happiness
+end
+
+def hygiene
+    @hygiene = hygiene
+end
+
+def 
